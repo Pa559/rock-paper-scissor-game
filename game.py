dfdc25433c500e7164fde277e300b561d6190a5a
Pa559 <pa559@georgetown.edu>
2021-09-21 11:51:09 -0400

    Update game.py

--- game.py
+++ game.py
@@ -19,23 +19,23 @@ print(user_choice)
 print("Computer Chose This")
 print(computer_choice)
 
-if ((computer_choice == rock) and (user_choice == scissor))
-print("Computer Wins!")
-if ((computer_choice == paper) and (user_choice == scissor))
-print("You Win!")
-if ((computer_choice == scissor) and (user_choice == scissor))
-print("Tie!")
+if ((computer_choice == rock) and (user_choice == scissor)):
+    print("Computer Wins!")
+if ((computer_choice == paper) and (user_choice == scissor)):
+    print("You Win!")
+if ((computer_choice == scissor) and (user_choice == scissor)):
+    print("Tie!")
 
-if ((computer_choice == rock) and (user_choice == rock))
-print("Tie!")
-if ((computer_choice == paper) and (user_choice == rock))
-print("Computer Wins!")
-if ((computer_choice == scissor) and (user_choice == rock))
-print("You Win!")
+if ((computer_choice == rock) and (user_choice == rock)):
+    print("Tie!")
+if ((computer_choice == paper) and (user_choice == rock)):
+    print("Computer Wins!")
+if ((computer_choice == scissor) and (user_choice == rock)):
+    print("You Win!")
 
-if ((computer_choice == rock) and (user_choice == paper))
-print("You Win!")
-if ((computer_choice == paper) and (user_choice == paper))
-print("Tie!")
-if ((computer_choice == scissor) and (user_choice == paper))
-print("Computer Wins!")
+if ((computer_choice == rock) and (user_choice == paper)):
+    print("You Win!")
+if ((computer_choice == paper) and (user_choice == paper)):
+    print("Tie!")
+if ((computer_choice == scissor) and (user_choice == paper)):
+    print("Computer Wins!")
